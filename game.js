4c6cf377b4719207e3c16530e4e91e9419b8f3c2
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-24 07:50:01 -0400

    Add Coin class

--- game.js
+++ game.js
@@ -102,3 +102,22 @@ class Lava {
 
 Lava.prototype.size = new Vec(1, 1);
 
+// Coin is a non-moving element that can be collected by the player
+// a coin object stores a base position as well as a wobble property
+class Coin {
+  constructor(pos, basePos, wobble) {
+    this.pos = pos;
+    this.basePos = basePos;
+    this.wobble = wobble;
+  }
+
+  get type() { return "coin"; }
+
+  static create(pos) {
+    let basePos = pos.plus(new Vec(0.2, 0.1));
+    return new Coin(basePos, basePos,
+                    Math.random() * Math.PI * 2);
+  }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
