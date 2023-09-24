08bdf2f97554e9d273b083517da335f10e37e79a
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-24 07:39:40 -0400

    Add Lava class

--- game.js
+++ game.js
@@ -77,3 +77,28 @@ class Player {
 }
 
 Player.prototype.size = new Vec(0.8, 1.5);
+
+
+// Lava is a moving element that kills the player when touched
+class Lava {
+  constructor(pos, speed, reset) {
+    this.pos = pos;
+    this.speed = speed;
+    this.reset = reset;
+  }
+
+  get type() { return "lava"; }
+
+  static create(pos, ch) {
+    if (ch == "=") {
+      return new Lava(pos, new Vec(2, 0));
+    } else if (ch == "|") {
+      return new Lava(pos, new Vec(0, 2));
+    } else if (ch == "v") {
+      return new Lava(pos, new Vec(0, 3), pos);
+    }
+  }
+}
+
+Lava.prototype.size = new Vec(1, 1);
+
