971b9a0fdf5a3b281710c01d711f705a70679c0e
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-25 19:25:51 -0400

    Add update to Player

--- game.js
+++ game.js
@@ -310,3 +310,31 @@ Coin.prototype.update = function(time) {
                   this.basePos, wobble);
 };
 
+
+// Player motion is handled separately per axis because hitting the floor should not 
+// prevent horizontal motion, and hitting a wall should not stop falling or jumping motion.
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+
+Player.prototype.update = function(time, state, keys) {
+  let xSpeed = 0;
+  if (keys.ArrowLeft) xSpeed -= playerXSpeed;
+  if (keys.ArrowRight) xSpeed += playerXSpeed;
+  let pos = this.pos;
+  let movedX = pos.plus(new Vec(xSpeed * time, 0));
+  if (!state.level.touches(movedX, this.size, "wall")) {
+    pos = movedX;
+  }
+
+  let ySpeed = this.speed.y + time * gravity;
+  let movedY = pos.plus(new Vec(0, ySpeed * time));
+  if (!state.level.touches(movedY, this.size, "wall")) {
+    pos = movedY;
+  } else if (keys.ArrowUp && ySpeed > 0) {
+    ySpeed = -jumpSpeed;
+  } else {
+    ySpeed = 0;
+  }
+  return new Player(pos, new Vec(xSpeed, ySpeed));
+};
