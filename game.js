2b18084538b1499afbf5dc68304db9b6edb16662
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-09-24 21:58:28 -0400

    Add scrollPlayerIntoView

--- game.js
+++ game.js
@@ -193,3 +193,31 @@ DOMDisplay.prototype.syncState = function(state) {
   this.dom.className = `game ${state.status}`;
   this.scrollPlayerIntoView(state);
 };
+
+
+// change the scroll position by manipulating that element’s 
+// scrollLeft and scrollTop properties when the player is too close to the edge
+DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
+  let width = this.dom.clientWidth;
+  let height = this.dom.clientHeight;
+  let margin = width / 3;
+
+  // The viewport
+  let left = this.dom.scrollLeft, right = left + width;
+  let top = this.dom.scrollTop, bottom = top + height;
+
+  let player = state.player;
+  let center = player.pos.plus(player.size.times(0.5))
+                         .times(scale);
+
+  if (center.x < left + margin) {
+    this.dom.scrollLeft = center.x - margin;
+  } else if (center.x > right - margin) {
+    this.dom.scrollLeft = center.x + margin - width;
+  }
+  if (center.y < top + margin) {
+    this.dom.scrollTop = center.y - margin;
+  } else if (center.y > bottom - margin) {
+    this.dom.scrollTop = center.y + margin - height;
+  }
+};
